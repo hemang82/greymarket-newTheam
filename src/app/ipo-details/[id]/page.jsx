aa15bef0f85a019aaaa2ac6e6223ad3a52f0d8aa
hemang82 <109712867+hemang82@users.noreply.github.com>
2025-11-20 14:23:04 +0530

    Change code

--- src/app/ipo-details/[id]/page.jsx
+++ src/app/ipo-details/[id]/page.jsx
@@ -6,14 +6,15 @@ import React from 'react'
 
 export default async function page({ params }) {
 
-  // console.log('====================================');
-  // console.log(params);
-  // console.log('====================================');
+  // =============== SEO ===================;
+  // title : {{companyName}} IPO Details {{year}} – Price, GMP, Dates, Allotment, Review
+  // Description : Get complete information on the {{companyName}} IPO including price band, issue size, dates, lot size, subscription status, GMP, allotment date, and listing details. Check latest updates, tips, and important insights before applying.
 
   const { id } = await params;
   const IPODetailsResponse = await getIPODetailsServer({ id: id });
 
   return (<>
+
     <Head>
       <title>IPO Details</title>
     </Head>
