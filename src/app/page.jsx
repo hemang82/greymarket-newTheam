656580860ef162a7f14bb272c831d9855dbd09b1
hemang82 <hemangchandekar82@gmail.com>
2025-12-02 19:51:52 +0530

    uodate home page for the tasting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -76,7 +76,7 @@ export default async function Home({ searchParams }) {
           icon: "tabler:arrow-right",
           label: "😻 Learn What's New",
         }}
-        title="Live/Upcoming IPOs"
+        title="CI/CD-test/Upcoming IPOs"
         description="Explore all live and upcoming IPOs with detailed insights - issue price, dates, lot size, and Grey Market Premium (GMP) updates."
         buttons={[
           {
