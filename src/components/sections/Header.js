17d9605e65c0a1959d0259eaeab8eee9b4f1191a
hemang82 <109712867+hemang82@users.noreply.github.com>
2025-12-04 11:57:28 +0530

    Update Searching code

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -10,7 +10,7 @@ import { getSearchIPO } from "@/api";
 import { STORAGE_KEYS } from "@/app_config/CommonVariable";
 import { getLocalStorage, removeLocalStorage } from "@/app_config/CommonFunction";
 import AuthMenu from "./AuthMenu";
-import { ipoDetailsNavigation } from "../cards/PricingCard";
+import { ipoDetailsNavigation, ipoDetailsNavigationPrefetch } from "../cards/PricingCard";
 
 export default function ClientOnly({ children, fallback = null }) {
   const [mounted, setMounted] = useState(false);
@@ -156,6 +156,7 @@ export function Header({ logo, links, buttons, className, ...rest }) {
   function handleSelect(item) {
     setQuery("");
     setShowDropdown(false);
+    ipoDetailsNavigationPrefetch(router, item.symbol)
     ipoDetailsNavigation(router, item.symbol);
   }
 
@@ -287,7 +288,6 @@ export function Header({ logo, links, buttons, className, ...rest }) {
                 className="w-72 lg:w-80 px-9 py-2 rounded-md border border-neutral-300 dark:border-neutral-800 bg-white dark:bg-neutral-900 text-sm placeholder:text-neutral-400 focus:outline-none focus:ring-1 focus:ring-[#135c33] header_search"
                 aria-label="Search for a company"
               />
-
               {query && (
                 <button
                   type="button"
@@ -298,7 +298,6 @@ export function Header({ logo, links, buttons, className, ...rest }) {
                   ✕
                 </button>
               )}
-
               {/* dropdown */}
               {showDropdown && (
                 <ul className="absolute right-0 left-0 mt-2 bg-white dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-800 rounded-md shadow-lg max-h-64 overflow-auto z-50">
